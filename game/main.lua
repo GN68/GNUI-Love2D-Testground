d97ff3560c9de23272b3749edce67b58f1c4cf87
GN68 <81744903+GN68@users.noreply.github.com>
2026-02-08 06:39:28 +0800

    some boilerplate

--- game/main.lua
+++ game/main.lua
@@ -1,8 +1,18 @@
+
+for _, item in pairs(love.filesystem.getDirectoryItems("core")) do
+	if item:find("%.lua$") then
+		require("core."..item:sub(1,-5))
+	end
+end
+
+local GNUI = require("lib.GNUI")
+
+print(GNUI)
+
 function love.load()
 end
 
 function love.update()
-	print("Hello!")
 end
 
 function love.draw()
